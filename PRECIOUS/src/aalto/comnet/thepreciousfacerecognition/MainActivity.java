c9ae9b4349cd2892d7e9f2cfb497d785697e5598
Todor Ginchev <todor.a.ginchev@aalto.fi>
2015-04-29 15:45:09 +0300

    Face detection improved

--- PRECIOUS/src/aalto/comnet/thepreciousfacerecognition/MainActivity.java
+++ PRECIOUS/src/aalto/comnet/thepreciousfacerecognition/MainActivity.java
@@ -6,13 +6,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
@@ -136,7 +140,7 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		}
         //convert bitmap format in OpenCV-compatible Mat format
         Utils.bitmapToMat(bmp, inputMat);
-        whiteBalance(inputMat);
+        //whiteBalance(inputMat);        
     	detectFace();
     	
     	if(toast_size)
@@ -169,61 +173,69 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     	return;
     }
     	
-    for (Rect rect : faceDetections.toArray()) {            
-        inputMat = inputMat.submat(rect);
-        Vector<Mat> channels = new Vector<Mat>(3);
-        Core.split(inputMat, channels); 
-        Mat iR = channels.get(0);
-        Mat iG = channels.get(1);
+    for (Rect rect : faceDetections.toArray()) {       
+        Rect rect2 = new Rect(rect.x, rect.y+rect.height/2, rect.width, rect.height/4);
         Mat aux = new Mat();
-        Core.subtract(iR, iG, aux);
-        Scalar mean = Core.mean(aux);
-        //Imgproc.threshold(aux, aux, 200, 255, Imgproc.THRESH_BINARY+Imgproc.THRESH_OTSU);
-        Imgproc.threshold(aux, aux, mean.val[0]/2.5, 255, Imgproc.THRESH_BINARY);
+        aux = inputMat.submat(rect2);
+        inputMat = inputMat.submat(rect);
+        //aux=filterSpectrumBW(aux);   
+        borderDetection(aux, 40, 3, 3);
+        inputMat=aux;
+        //        Vector<Mat> channels = new Vector<Mat>(3);
+//        Core.split(inputMat, channels); 
+//        Mat iR = channels.get(0);
+//        Mat iG = channels.get(1);
+//        Mat aux = new Mat();
+//        Core.subtract(iR, iG, aux);
+//        Scalar mean = Core.mean(aux);
+//        //Imgproc.threshold(aux, aux, 200, 255, Imgproc.THRESH_BINARY+Imgproc.THRESH_OTSU);
+//        Imgproc.threshold(aux, aux, mean.val[0]/2.5, 255, Imgproc.THRESH_BINARY);
+//
+//        int kernelSize=3;
+//        Imgproc.dilate(aux, aux, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(kernelSize, kernelSize)) );
+//        Imgproc.erode(aux, aux, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(kernelSize, kernelSize)) );
+//       
 
-        int kernelSize=3;
-        Imgproc.dilate(aux, aux, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(kernelSize, kernelSize)) );
-        Imgproc.erode(aux, aux, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(kernelSize, kernelSize)) );
-       
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();   
-	    Mat hierarchy=new Mat();
-        Imgproc.findContours(aux, contours,hierarchy, Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);        	
-	    for(int i=0; i< contours.size();i++){
-	    	double contourArea=Imgproc.contourArea(contours.get(i));
-	    	//Look for objects with are bigger than minObjectArea pixels 
-	        if (contourArea > (aux.width()*aux.height()/4) )
-	        	Imgproc.drawContours(inputMat, contours, i, new Scalar(255,0,0),1);    	                    
-	    }
-	    Rect rect3 = rect;
-	    rect3.height=rect.height*1/3;
-	    Rect roi = new Rect(0,aux.height()*2/3-1,aux.width(),aux.height()*1/3-1);
-	    aux = new Mat(aux, roi);
-	    contours = new ArrayList<MatOfPoint>();   
-        Imgproc.findContours(aux, contours,hierarchy, Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);        	
-	    for(int i=0; i< contours.size();i++){
-	    	double contourArea=Imgproc.contourArea(contours.get(i));
-	    	//Look for objects with are bigger than minObjectArea pixels 
-	        if (contourArea > (aux.width()*aux.height()/4) ){
-	        	//Imgproc.drawContours(inputMat, contours, i, new Scalar(255,0,0),1);
-	        	Rect BBrect = Imgproc.boundingRect(contours.get(i));
-	            TextView tv = (TextView) findViewById(R.id.textViewFaceDetection);
-	            tv.setText("Face size = "+BBrect.width+"x"+inputMat.height()+"."+"\nRatio = "+(float)inputMat.height()/BBrect.width+".");
-	            Core.rectangle(inputMat, new Point(BBrect.x,inputMat.height()*2/3), new Point(BBrect.x+BBrect.width,inputMat.height()*2/3+BBrect.height/3), new Scalar(255,255,0));
-	        }            
-	    }        
+//        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();   
+//	    Mat hierarchy=new Mat();
+//        Imgproc.findContours(aux, contours,hierarchy, Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);        	
+//	    for(int i=0; i< contours.size();i++){
+//	    	double contourArea=Imgproc.contourArea(contours.get(i));
+//	    	//Look for objects with are bigger than minObjectArea pixels 
+//	        if (contourArea > (aux.width()*aux.height()/4) )
+//	        	Imgproc.drawContours(inputMat, contours, i, new Scalar(255,0,0),1);    	                    
+//	    }
+//	    Rect rect3 = rect;
+//	    rect3.height=rect.height*1/3;
+//	    Rect roi = new Rect(0,aux.height()*2/3-1,aux.width(),aux.height()*1/3-1);
+//	    aux = new Mat(aux, roi);
+//	    contours = new ArrayList<MatOfPoint>();   
+//        Imgproc.findContours(aux, contours,hierarchy, Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);        	
+//	    for(int i=0; i< contours.size();i++){
+//	    	double contourArea=Imgproc.contourArea(contours.get(i));
+//	    	//Look for objects with are bigger than minObjectArea pixels 
+//	        if (contourArea > (aux.width()*aux.height()/4) ){
+//	        	//Imgproc.drawContours(inputMat, contours, i, new Scalar(255,0,0),1);
+//	        	Rect BBrect = Imgproc.boundingRect(contours.get(i));
+//	            TextView tv = (TextView) findViewById(R.id.textViewFaceDetection);
+//	            tv.setText("Face size = "+BBrect.width+"x"+inputMat.height()+"."+"\nRatio = "+(float)inputMat.height()/BBrect.width+".");
+//	            Core.rectangle(inputMat, new Point(BBrect.x,inputMat.height()*2/3), new Point(BBrect.x+BBrect.width,inputMat.height()*2/3+BBrect.height/10), new Scalar(255,255,0));
+//	        }            
+//	    }       
+    
     ImageView im = (ImageView) findViewById(R.id.imageView_face);
     Bitmap bmp_out = Bitmap.createBitmap(inputMat.width(), inputMat.height(), Config.ARGB_8888);
     Utils.matToBitmap(inputMat, bmp_out); 
     im.setImageBitmap(bmp_out);
-}
-
+    }
+} 
 /**
  * 
  * @param in
  * @param out
  * @throws IOException
  */
-}            
+           
 private static boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
     InputStream in = null;
     OutputStream out = null;
@@ -320,6 +332,178 @@ public void whiteBalance (Mat input){
 			data[2] = (data[2]-bottomB)*255/(topB-bottomB);			
     		input.put(j, i, data);
     	}    
+}
+
+/**
+ * 
+ * @param detectedObject
+ * @param colorName
+ * @param i
+ */
+public Mat filterSpectrumBW (Mat detectedObject){
+	int SL=40,SH=256, VL=40,VH=256;
+	Imgproc.cvtColor(detectedObject, detectedObject, Imgproc.COLOR_RGB2HSV);
+	//Get the Hue Sat and Val channels from the detectedObject matrix
+	List<Mat> channels = new Vector<Mat>(3);
+	Core.split(detectedObject, channels); 
+	
+	//Get only the Hue channel, it contains the color spectrum information
+	List<Mat> matList = new LinkedList<Mat>();
+	matList.add(channels.get(0));
+	//Calculate histogram
+	Mat histogram = new Mat();
+	MatOfFloat ranges=new MatOfFloat(0,256);
+	Imgproc.calcHist(
+	        matList, 
+	        new MatOfInt(0), 
+	        new Mat(), 
+	        histogram , 
+	        new MatOfInt(256), 
+	        ranges);
+	
+	Log.i("FOOD_INTAKE","Object "+":"+"histogram= "+histogram.dump());
+	histogram.put(0, 0, 0);
+	//Search for the maximum Hue value
+	Core.MinMaxLocResult minMax=Core.minMaxLoc(histogram);
+	Log.i("FOOD_INTAKE","Object "+" Max="+  minMax.maxVal+" Loc="+minMax.maxLoc);
+	//            Log.i("FOOD_INTAKE","Object "+i+" His Size="+ histogram.size());
+	//            double[] aaaaa = histogram.get(27,0);
+	//            Log.i("FOOD_INTAKE","Object "+i+" His val="+ aaaaa[0]);
+	
+	//Obtain BW
+	//Do not take into account Hue=0 or Hue=179, start from 1 or from 178
+	int Hfreq = (int)minMax.maxLoc.y;
+	do{
+		if(Hfreq>=179)
+			Hfreq=1;
+		else
+			Hfreq++;
+	}while ( (histogram.get(Hfreq,0)[0]> (minMax.maxVal/10)) &&
+			( (histogram.get(Hfreq,0)[0]>(minMax.maxVal/5)) || (histogram.get(Hfreq+1,0)[0]>(histogram.get(Hfreq,0)[0])) ) );            
+	int Lfreq = (int)minMax.maxLoc.y;
+	do{
+		if(Lfreq<=1)
+			Lfreq=178;
+		else
+			Lfreq--;
+	}while ( (histogram.get(Lfreq,0)[0]> (minMax.maxVal/10)) &&
+			( (histogram.get(Lfreq,0)[0]>(minMax.maxVal/5)) || (histogram.get(Lfreq+1,0)[0]>(histogram.get(Lfreq,0)[0])) ) );
+	Log.i("FOOD_INTAKE","Object "+" BW= "+Hfreq+"-"+Lfreq);
+	
+	//Convert to RGB color space again
+	Imgproc.cvtColor(detectedObject, detectedObject, Imgproc.COLOR_HSV2RGB);
+	            	
+		
+	/*
+	 * Filter color spectrum according to max Hue location and BW
+	 */
+	int OpenCloseSES=0;
+	Mat detectedColorMask = new Mat();
+	
+	if(Lfreq<Hfreq){
+		detectedColorMask = filterColor(detectedObject, Lfreq, Hfreq, SL, SH, VL, VH, OpenCloseSES);
+	}
+	else{ 
+		//Only for orange-red-violet transition
+		Mat detectedColorMask1 = new Mat();
+		Mat detectedColorMask2 = new Mat();
+		detectedColorMask1 = filterColor(detectedObject, Lfreq, 179, SL, SH, VL, VH, OpenCloseSES);
+		detectedColorMask2 = filterColor(detectedObject, 0, Hfreq, SL, SH, VL, VH, OpenCloseSES);
+		Core.add(detectedColorMask1, detectedColorMask2, detectedColorMask);
+	}
+	
+//	if(DEBUG){
+//	  	Bitmap bmpOutputProcessColor = Bitmap.createBitmap(detectedObject.width(), detectedObject.height(), Bitmap.Config.RGB_565);
+//	  	Utils.matToBitmap(detectedColorMask, bmpOutputProcessColor);  
+//	  	BitmapImage.add (bmpOutputProcessColor);
+//	  	ImageName.add("Detected color mask " + colorName); 
+//	}	   		    		
+	
+	//Mat detectedColorMaskout = new Mat();
+	//detectedObject.copyTo(detectedColorMaskout,detectedColorMask);
+	return detectedColorMask;
+}
+/**
+ * 
+ * @param image
+ * @param hL
+ * @param hH
+ * @param sL
+ * @param sH
+ * @param vL
+ * @param vH
+ * @param OpenCloseSES
+ * @return
+ */
+private Mat filterColor(Mat image, int hL,int hH,int sL,int sH,int vL,int vH, int OpenCloseSES){
+	//Convert RGB matrix into HSV matrix
+	Mat hsvimage = new Mat(image.cols(),image.rows(),CvType.CV_8UC3);
+	hsvimage.setTo(new Scalar(0,0,0));
+	Imgproc.cvtColor(image, hsvimage, Imgproc.COLOR_RGB2HSV);
+	Mat binImage = new Mat();     
+	Core.inRange(hsvimage, new Scalar(hL, sL, vL), new Scalar(hH, sH, vH), binImage); //Threshold the image
+	
+	if (OpenCloseSES>0){
+		//morphological opening (remove small objects from the foreground)
+		Imgproc.erode(binImage, binImage, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(OpenCloseSES, OpenCloseSES)) );
+		Imgproc.dilate( binImage, binImage, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(OpenCloseSES, OpenCloseSES)) ); 
+		//morphological closing (fill small holes in the foreground)
+		Imgproc.dilate( binImage, binImage, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(OpenCloseSES, OpenCloseSES)) ); 
+		Imgproc.erode(binImage, binImage, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(OpenCloseSES, OpenCloseSES)) );
+	}
+	return binImage;	
+}
+
+public Mat detectSkin(Mat input){
+	Mat output = new Mat();
+	//Convert to RGB color space again
+	int Y_MIN  = 0;
+	int Y_MAX  = 255;
+	int Cr_MIN = 133;
+	int Cr_MAX = 173;
+	int Cb_MIN = 77;
+	int Cb_MAX = 127;
+	Imgproc.cvtColor(input, output, Imgproc.COLOR_RGB2YCrCb);
+	Core.inRange(output, new Scalar(Y_MIN,Cr_MIN,Cb_MIN), new Scalar(Y_MAX,Cr_MAX,Cb_MAX), output); //Threshold the image
+	return output;
+}
+
+/**
+ * 
+ * @param image
+ * @param threshold
+ * @param EEsize
+ * @param BlurSize
+ * @return
+ */
+public Mat borderDetection (Mat image, int threshold, int EEsize, int BlurSize){	
+	//Mat output=image;//Store input image
+	int ratio = 3;	
+	/// Reduce noise with a kernel 3x3	
+	Mat detected_edges=image.clone();
+	
+	if(BlurSize>0)
+		Imgproc.blur( image, detected_edges, new Size(BlurSize,BlurSize) );
+
+	/// Canny detector
+	Imgproc.Canny( detected_edges, detected_edges, (double)threshold, (double)threshold*ratio);
+
+	if (EEsize>0)
+		Imgproc.dilate( detected_edges, detected_edges, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(EEsize, EEsize)) ); 
+
+	//Delete corners from the original image
+	double []border = new double[4];
+	border[0]=0;border[1]=0;border[2]=0;border[3]=0;
+	
+	for (int x=0;x<image.rows();x++)
+		for (int y=0;y<image.cols();y++){
+			double []data = detected_edges.get(x,y);
+			boolean Delete = (int)data[0]!=0;
+			if(Delete){	
+				image.put(x,y,border);
+			}
+		} 	
+	return image;
 }
 
 }
