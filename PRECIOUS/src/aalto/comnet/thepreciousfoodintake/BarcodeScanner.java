483073d391c4b8441d75b4aa93210daa7efdbff7
Todor Ginchev <todor.a.ginchev@aalto.fi>
2015-06-02 10:46:46 +0300

    Barcode scanner database added

--- PRECIOUS/src/aalto/comnet/thepreciousfoodintake/BarcodeScanner.java
+++ PRECIOUS/src/aalto/comnet/thepreciousfoodintake/BarcodeScanner.java
@@ -1,6 +1,8 @@
 package aalto.comnet.thepreciousfoodintake;
 
 
+import org.apache.http.HttpResponse;
+
 import aalto.comnet.thepreciousproject.R;
 import android.app.Activity;
 import android.content.Intent;
@@ -27,7 +29,49 @@ public class BarcodeScanner extends Activity {
         integrator.addExtra("RESULT_DISPLAY_DURATION_MS", 3000L);
         integrator.addExtra("PROMPT_MESSAGE", "Custom prompt to scan a product");
         integrator.initiateScan(IntentIntegrator.PRODUCT_CODE_TYPES);
+	    
     }
+    
+//    private static final String HOST_NAME = "api.outpan.com";
+//    private static final String URL = "http://www.outpan.com/api/get_product.php?barcode=5449000000996";
+//    private static final String USER_NAME = "b428a2419f57a7dfa196e99b2ba23e9a:";
+//    //private static final String PASSWORD = ":";
+//    private void basicAuthDemo() {
+//    	DefaultHttpClient httpclient = new DefaultHttpClient();
+//	      int SDK_INT = android.os.Build.VERSION.SDK_INT;
+//	      if (SDK_INT > 8) 
+//	      {
+//	    	  	StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+//	                  .permitAll().build();
+//	          	StrictMode.setThreadPolicy(policy);
+//		        try {
+//		            httpclient.getCredentialsProvider().setCredentials(
+//		                    new AuthScope(HOST_NAME, 443),
+//		                    new UsernamePasswordCredentials(USER_NAME));
+//		 
+//		            HttpGet httpget = new HttpGet(URL);
+//		 
+//		            System.out.println("executing request" + httpget.getRequestLine());
+//		            HttpResponse response = httpclient.execute(httpget);
+//		            HttpEntity entity = response.getEntity();
+//		 
+//		            System.out.println("----------------------------------------");
+//		            System.out.println();
+//		            if (entity != null) {
+//		                System.out.println("Response content length: " + entity.getContentLength());
+//		                System.out.println(EntityUtils.toString(entity));
+//		            }
+//		        } catch(Exception e){
+//		        	e.printStackTrace();
+//		        	Log.e("TAG","",e);
+//		        }finally {
+//		            // When HttpClient instance is no longer needed,
+//		            // shut down the connection manager to ensure
+//		            // immediate deallocation of all system resources
+//		            httpclient.getConnectionManager().shutdown();
+//		        }
+//	      }
+//    }
 
     
     
@@ -58,7 +102,7 @@ public class BarcodeScanner extends Activity {
 //	    startActivity(launchactivity);  
 	    WebView webView = (WebView) findViewById(R.id.web_view);
 	    webView.setWebViewClient(new WebViewClient());
-	    webView.loadUrl("http://www.google.com/search?as_q="+barcode);
+	    webView.loadUrl("http://www.ean-search.org/perl/ean-search.pl?q="+barcode);
 	    
 //	    Uri uri = Uri.parse("http://www.google.com/search?as_q="+barcode);//"http://www.google.com/#q=fish");
 //	    Intent i = new Intent(Intent.ACTION_VIEW, uri);
