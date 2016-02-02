1679e65d45cccee6fbef52d6b9afb2a88ecf38ef
TodorGinchev <todor.a.ginchev@aalto.fi>
2016-02-02 21:33:49 +0200

    Navitagion Drawer Proto Finished, Profile Picture in Main screen done, Toolbar with circular profile photo done

--- PRECIOUS/app/src/main/java/aalto/comnet/precious/main/MainActivity.java
+++ PRECIOUS/app/src/main/java/aalto/comnet/precious/main/MainActivity.java
@@ -1,6 +1,7 @@
 package aalto.comnet.precious.main;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,23 +11,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+
+    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    private LinearLayout mTitleContainer;
+    private TextView mTitle;
+    private AppBarLayout mAppBarLayout;
+    private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Main screen toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //toolbar.setLogo(R.drawable.precious_icon);
-        //toolbar.setSubtitle("This is The PRECIOUS Project");
-        //toolbar.setTitle("Welcome");
+        bindActivity();
+
+        mToolbar.setTitle("");
+        //mAppBarLayout.addOnOffsetChangedListener(this);//TODO this must be uncommented
+
+        setSupportActionBar(mToolbar);
+        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +57,8 @@ public class MainActivity extends AppCompatActivity
         //This is from the navigation drawer view
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+                this, drawer, mToolbar, R.string.navigation_drawer_open,
+                    R.string.navigation_drawer_close){
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
@@ -67,16 +85,15 @@ public class MainActivity extends AppCompatActivity
         TextView tv_location = (TextView) header.findViewById(R.id.textViewNavDrawLocation);
         tv_location.setText("Espoo, Finland");
 
-
-
-
-
-
-
-
-
-
-
+        //Set main profile photo and text
+        ImageView iv_profile_main = (ImageView) findViewById(R.id.main_logo);
+        iv_profile_main.setImageResource(R.drawable.precious_logo_main);
+        TextView tv_main_username = (TextView) findViewById(R.id.main_tv_username);
+        tv_main_username.setText("Mr. Anderson");
+        TextView tv_main_location = (TextView) findViewById(R.id.main_tv_location);
+        tv_main_location.setText("Espoo, Finland");
+        ImageView iv_main_profile = (ImageView) findViewById(R.id.main_imageview_profile);
+        iv_main_profile.setImageResource(R.drawable.profile);
 
 
 
@@ -93,7 +110,76 @@ public class MainActivity extends AppCompatActivity
 //        double image1_hw_relation=image1.getBitmap().getHeight()/image1.getBitmap().getWidth(); //relation = height/width
 //        Log.i("HW",image1_hw_relation+" "+image1.getBitmap().getHeight()+" "+image1.getBitmap().getWidth());
 //        imageButton1.setLayoutParams(new LinearLayout.LayoutParams((int) (0.9 * width), (int) (0.9 * width * image1_hw_relation)));
+
+
     }
+
+    private void bindActivity() {
+        mToolbar        = (Toolbar) findViewById(R.id.main_toolbar);
+        mTitle          = (TextView) findViewById(R.id.main_textview_title);
+        mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
+        mAppBarLayout   = (AppBarLayout) findViewById(R.id.main_appbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //TODO this metod must be uncommented
+/*    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+*/
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if(mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+    public static void startAlphaAnimation (View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -103,28 +189,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -149,4 +213,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
