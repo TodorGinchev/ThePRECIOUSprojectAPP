055b090d375f10a8133fa4d78932b7a74d54e6ac
TodorGinchev <todor.a.ginchev@aalto.fi>
2016-03-14 10:55:26 +0200

    AES decryptor working

---
+++ JSONtest/app/src/main/java/preciousband/precious/comnet/aalto/myapplication/Encryptor.java
@@ -0,0 +1,75 @@
+package preciousband.precious.comnet.aalto.myapplication;
+
+
+import android.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class Encryptor {
+    public static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            System.out.println("encrypted string: "
+                    + Base64.encodeToString(encrypted,Base64.DEFAULT));//  encodeBase64String(encrypted));
+
+            return Base64.encodeToString(encrypted, Base64.DEFAULT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String decrypt(String key, String initVector, String encrypted) {
+        try {
+//            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            IvParameterSpec iv = new IvParameterSpec(hexStringToByteArray(initVector));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.decode(encrypted, Base64.DEFAULT));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    /**
+     *
+     */
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        String key = "Bar12345Bar12345"; // 128 bit key
+        String initVector = "RandomInitVector"; // 16 bytes IV
+
+        System.out.println(decrypt(key, initVector,
+                encrypt(key, initVector, "Hello World")));
+    }
+}
--- JSONtest/app/src/main/java/preciousband/precious/comnet/aalto/myapplication/MainActivity.java
+++ JSONtest/app/src/main/java/preciousband/precious/comnet/aalto/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -23,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     public static final String apiKey = "387de4f0-e79b-11e5-b938-9dea270b774d";
+    public static final String Key = "be2b9f48ecaf294c2fc68e2862501bbd";
+
+    //User info
+    private String email = "test1003@test.bg";
+    private String password = "mypasswds";
+    private int activityClass = 5;
+    private String nickname = "1003";
+    private String birthdate ="19June1441";
+    private int weight = 12;
+    private int height = 140;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 getUserInfo();
             }
         });
-        //sendRegistrationRequest();
     }
 
     /**
@@ -63,13 +75,6 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "Sending request");
         String serverURL = "http://precious2.research.netlab.hut.fi:9000/register";
-        String email = "test1000@test.bg";
-        String password = "mypass";
-        int activityClass = 5;
-        String nickname = "Walker, Johnnie";
-        String birthdate ="19June1440";
-        int weight = 12;
-        int height = 140;
         sendJson(serverURL, email, password, weight, height, activityClass, nickname, birthdate);
     }
 
@@ -85,7 +90,16 @@ public class MainActivity extends AppCompatActivity {
      [17:22:38] Helf Christopher: 4) RETREIVE RESPONSE
      [17:22:57] Helf Christopher: 5) CHECK RESPONSE HEADER „x-precious-encryption-iv“
 
-     KEY FOR DECRIPTION : be2b9f48ecaf294c2fc68e2862501bbd
+     [10:19:53] Helf Christopher: Encryption: Raw JSON -> Convert to String -> AES Encrypt -> Base64 Encoding
+     [10:20:23] Helf Christopher: Decryption: Base64 String -> Decode Base64 -> Decrypt -> UTF8 String -> Parse JSON
+     [10:20:43] Helf Christopher: As for Decryption: the IV received in the headers in in HEX
+     [10:20:48] Helf Christopher: the key is UTF8
+
+     KEY FOR DECRYPTION : be2b9f48ecaf294c2fc68e2862501bbd
+     T76egWy0jfKNjk3a6wMUaM7AhFh7M6nZlWtB/M2XtsmdA+nQi7eduoztR07WY9mR1y6OrPUtnAMSSmyoAPQm2gkZdYrTUr4zWNDvF/0H4iTPJwyN09Fgky7FIlQgA0Iu2MDis+2GqOqEPI4DRrGSiw==
+     336C674C79333965735A483334767859416463684B6558754C6252614735554B5A744D52412B42352B722F61596143637043486D443543426E526B434E65473875554F57423746414177664E4247725270774930776C44736A674A74563538714D654E676A53486C71492B6B737668794C69427870324C74536256707A7139576D4A646335616E71373334694A6355525979377158513D3D
+
+
 
      [17:25:12] Helf Christopher: initVector.getBytes("HEX")
      [17:25:17] Helf Christopher: instead of initVector.getBytes("UTF-8")
@@ -107,11 +121,11 @@ public class MainActivity extends AppCompatActivity {
 
             public void run() {
                 Looper.prepare(); //For Preparing Message Pool for the child Thread
+                JSONObject json = new JSONObject();
+
                 HttpClient client = new DefaultHttpClient();
                 HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
                 HttpResponse response;
-                JSONObject json = new JSONObject();
-
                 try {
                     HttpPost post = new HttpPost(serverURL);
                     json.put("email", email);
@@ -162,7 +176,27 @@ public class MainActivity extends AppCompatActivity {
 
                     /*Checking response */
                     if(response!=null){
-                        Log.i(TAG,"REPSPONSE IS:"+EntityUtils.toString(response.getEntity()));
+//                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                        response.getEntity().writeTo(baos);
+//                        byte[] bytes = baos.toByteArray();
+//                        Log.i(TAG,"RESPONSE IS:"+bytesToHex(bytes));
+                        Header [] headers = response.getAllHeaders();
+                        String iv="";
+                        for (int i=0; i<headers.length;i++) {
+                            if(headers[i].getName().equals("x-precious-encryption-iv"))
+                                iv = headers[i].getValue().toString();
+//                            Log.i(TAG, "Name: "+headers[i].getName());
+//                            Log.i(TAG, "toString: "+headers[i].toString());
+                        }
+                        byte [] ivHex = iv.getBytes("UTF-8");
+                        String message = EntityUtils.toString(response.getEntity());
+                        byte [] messageHex = message.getBytes("UTF-8");
+                        Log.i(TAG,"IV IS: "+iv);
+                        Log.i(TAG,"MESSAGE TEXT IS: "+message);
+                        Log.i(TAG, "MESSAGE HEX IS: " + bytesToHex(messageHex));
+                        Log.i(TAG,"KEY TEXT IS: "+Key);
+                        Log.i(TAG,"KEY HEX IS: "+bytesToHex(Key.getBytes("UTF-8")));
+                        Log.i(TAG,"Encrypted message is: "+Encryptor.decrypt(Key,iv,message));
                     }
                 } catch(Exception e) {
                     e.printStackTrace();
@@ -173,4 +207,19 @@ public class MainActivity extends AppCompatActivity {
         };
         t.start();
     }
+
+    /**
+     *
+     */
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 }
