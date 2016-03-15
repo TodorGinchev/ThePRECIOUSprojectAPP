7ecfdcad30eaa90843289d1d6bbbfda7e94603b9
TodorGinchev <todor.a.ginchev@aalto.fi>
2016-03-15 17:23:37 +0200

    Able to daw canvas circle

--- PRECIOUS/app/src/main/java/activity_tracker/precious/comnet/aalto/MountainViewActivity.java
+++ PRECIOUS/app/src/main/java/activity_tracker/precious/comnet/aalto/MountainViewActivity.java
@@ -2,183 +2,85 @@ package activity_tracker.precious.comnet.aalto;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
 
-import com.viewpagerindicator.CirclePageIndicator;
+import java.util.Random;
 
 import ui.precious.comnet.aalto.precious.R;
 
 public class MountainViewActivity extends AppCompatActivity {
     public static Context appConext;
     public static final String PREFS_NAME = "IRsubappPreferences";
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ATFragmentAdapter mAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mountain_view_activity);
+//        setContentView(new MountainView(this));
         appConext=getApplicationContext();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        //setSupportActionBar(toolbar);
-        toolbar.setTitle(getString(R.string.mountain_view__title));
-        toolbar.setTitleTextColor(getResources().getColor(R.color.mountainView));
+        drawMountainView();
 
 
-        toolbar.setNavigationIcon(R.drawable.ir_back);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        mAdapter = new ATFragmentAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mAdapter);
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-            @Override
-            public void onPageSelected(int position) {
-//                if (position == 1) {
-//                    LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(appConext);
-//                    Intent i = new Intent("IR2_REFRESH");
-//                    lbm.sendBroadcast(i);
-//                }
-            }
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-        CirclePageIndicator titleIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
-        titleIndicator.setViewPager(mViewPager);
     }
     /**
      *
      */
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        SharedPreferences preferences = this.getSharedPreferences(PREFS_NAME, 0);
-//        Log.i("SETTINGS:", preferences.getInt("selectedBox1", -1) + "" + preferences.getInt("selectedBox2", -1) + preferences.getInt("selectedBox3", -1) + preferences.getInt("selectedBox4", -1) + ":" + preferences.getInt("preferredBox1", -1));
-//    }
+    void drawMountainView(){
 
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.RelativeLayoutMountains);
+        rl.getLayoutParams().height = 900;  // change heigh of the layout
+        rl.setBackgroundColor(Color.RED);
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_outegoal_activity, menu);
-//        return true;
-//    }
+        MountainView mv;
+        Random randomGenerator = new Random();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.outcomegoal_activity, container, false);
-//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+        //FOr the achievements
+        for( int i=0; i<365; i++){
+            mv = new MountainView(this);
+            mv.setId(i);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100,100);
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            if(i>0) {
+                params.addRule(RelativeLayout.ALIGN_LEFT, i - 1);
+                params.setMargins((int) this.getResources().getDimension(R.dimen.distance_mountains), 0, 0, 0);
             }
-            return null;
+            mv.setLayoutParams(params);
+//            mv.bringToFront();
+            rl.addView(mv);
+        }
+
+
+        final HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
+        hsv.postDelayed(new Runnable() {
+            public void run() {
+                hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+            }
+        }, 500L);
+
+    }
+    /**
+     *
+     */
+    public class MountainView extends View {
+        public MountainView (Context context) {
+            super(context);
+        }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            //Dibujar aquí
+            Paint pincel = new Paint();
+            pincel.setColor(Color.BLUE);
+            pincel.setStrokeWidth(100);
+            pincel.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(50, 50, 100, pincel);
         }
     }
 }
