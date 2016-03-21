9398015390bf22f65013a43ee7fc6e5acf40aa16
TodorGinchev <todor.a.ginchev@aalto.fi>
2016-03-21 14:53:03 +0200

    Added onlick event that updates the mountain view

--- PRECIOUS/app/src/main/java/activity_tracker/precious/comnet/aalto/MountainViewActivity.java
+++ PRECIOUS/app/src/main/java/activity_tracker/precious/comnet/aalto/MountainViewActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.RelativeLayout;
@@ -21,22 +22,35 @@ import java.util.Random;
 
 import ui.precious.comnet.aalto.precious.R;
 
-public class MountainViewActivity extends AppCompatActivity {
+public class MountainViewActivity extends AppCompatActivity implements View.OnTouchListener {
     public static Context appConext;
     public static final String PREFS_NAME = "IRsubappPreferences";
     public static final String TAG = "MountainViewActivity";
     private static int screen_width;
     private static int screen_height;
-    private static final int NUM_MOUNTAINS=60; //TODO find optimal value
+    private static final int NUM_MOUNTAINS=5; //TODO find optimal value
     private static int mountain_width;
     private static int layout_width;
     private static int layout_height; // depends on variable screen_height
     private static int mountain_top_margin;
 
+    private int [] PA_data = new int[NUM_MOUNTAINS];
+    private int [] Goals_data = new int[NUM_MOUNTAINS];
+
+    private Path circle1;
+    private float circle1_pos=0;
+    private Path circle2;
+    private float circle2_pos=0;
+    private MountainView mv;
+
+    private HorizontalScrollView hsv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mountain_view_activity);
+
+        hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
 
         //Set toolbar title and icons
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -60,8 +74,10 @@ public class MountainViewActivity extends AppCompatActivity {
         int screen_height = size.y;
         layout_height = (int) 2*screen_height/3; //900
         mountain_width = screen_width/3;
-        layout_width = (int)(2*mountain_width/3)*(NUM_MOUNTAINS+2);//90000;
+        layout_width = (int)(mountain_width+(2*mountain_width/3)*(NUM_MOUNTAINS-3)+mountain_width*2);//90000;
         mountain_top_margin = screen_height/10;
+        //Generate random PA data
+        generatePAdata();
         //Draw mountains
         appConext=getApplicationContext();
         drawMountainView();
@@ -70,40 +86,27 @@ public class MountainViewActivity extends AppCompatActivity {
      *
      */
     void drawMountainView(){
-        final HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
+
 //        hsv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,layout_height));
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.RelativeLayoutMountains);
         rl.getLayoutParams().height = layout_height;  // change height of the layout
         rl.getLayoutParams().width = layout_width;  // change width of the layout
         rl.setBackgroundColor(0xaab3e5fc);
+        //Set click listener for the scroll view
+        hsv.setOnTouchListener(this);
 
-
-        MountainView mv = new MountainView(this);
+        mv = new MountainView(this);
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(layout_width,layout_height); //RelativeLayout.LayoutParams.WRAP_CONTENT);
         mv.setLayoutParams(params);
 //            mv.bringToFront();
         rl.addView(mv);
-//        //FOr the achievements
-//        for( int i=0; i<365; i++){
-//            mv = new MountainView(this);
-//            mv.setId(i);
-//            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(300, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-//            if(i>0) {
-//                params.addRule(RelativeLayout.ALIGN_LEFT, i - 1);
-////                params.setMargins((int) this.getResources().getDimension(R.dimen.distance_mountains), 0, 0, 0);
-//                params.setMargins(200, 100, 100, 100);
-//            }
-//            mv.setLayoutParams(params);
-////            mv.bringToFront();
-//            rl.addView(mv);
-//        }
 
 
 //        final HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
         hsv.postDelayed(new Runnable() {
             public void run() {
-                hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+//                hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+                hsv.scrollTo(layout_width-mountain_width*4,0);
             }
         }, 500L);
 
@@ -112,6 +115,9 @@ public class MountainViewActivity extends AppCompatActivity {
      *
      */
     public class MountainView extends View {
+        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
+        Path pth = new Path();
+
         public MountainView (Context context) {
             super(context);
         }
@@ -120,121 +126,79 @@ public class MountainViewActivity extends AppCompatActivity {
             int w = getWidth();
             int h = getHeight();
             Log.i(TAG, "H= " + h + " W= " + w);
-            Paint p;// = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-            Path pth;// = new Path();
 
-//            int x0=49700;
-//            int triangle_width=250;
-//            int triangle_height=600;
-//            pth.moveTo(x0, 900);
-//            pth.lineTo(x0 + triangle_width, 900);
-//            pth.lineTo(x0+triangle_width/2,900-triangle_height);
-//            pth.lineTo(x0, 900);
-
-//            p.setShader(new LinearGradient(0, 900, 0, 900-triangle_height, 0xff689f38, 0xffdcedc8, Shader.TileMode.CLAMP));
-//            p.setShader(new LinearGradient(x0,y0,x1,y1,0xff000000, 0xffffffff, Shader.TileMode.CLAMP));
-//            canvas.drawPath(pth,p);
-
-
-//            int triangle_width=(int)((1.5)*(w/NUM_MOUNTAINS));
-
-            int triangle_height;
-            Random randomGenerator = new Random();
 //            Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.precious_icon);
 //            = b.getHeight();
 //            int mountain_top_margin
             int x0_triangle;
+            LinearGradient mountainShader;
             for (int i=0; i<NUM_MOUNTAINS; i++){
+                //Draw mountain
+                p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
+                pth = new Path();
                 if(i>NUM_MOUNTAINS-3) {
                     x0_triangle = (NUM_MOUNTAINS - 3) * mountain_width * 2 / 3 + (i - (NUM_MOUNTAINS - 3)) * mountain_width;
+                    p.setShader(new LinearGradient(x0_triangle, 0, x0_triangle + mountain_width, 0, 0x77dcedc8, 0x77689f38, Shader.TileMode.CLAMP));
+                }
+                else{
+                    x0_triangle = i * mountain_width * 2 / 3;
+                    p.setShader(new LinearGradient(x0_triangle, 0, x0_triangle + mountain_width, 0, 0xffdcedc8, 0xff689f38, Shader.TileMode.CLAMP));
+                }
+                pth.moveTo(x0_triangle, h);
+                pth.lineTo(x0_triangle + mountain_width, h);
+                pth.lineTo(x0_triangle + mountain_width / 2, h - PA_data[i]);
+                pth.lineTo(x0_triangle, h);
+                canvas.drawPath(pth, p);
 
-                    //Draw mountain
-                    p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-                    pth = new Path();
-                    triangle_height = randomGenerator.nextInt(h-10-mountain_top_margin) + 10; //random number between 10 and reward icon height
-                    pth.moveTo(x0_triangle, h);
-                    pth.lineTo(x0_triangle + mountain_width, h);
-                    pth.lineTo(x0_triangle + mountain_width/2, h - layout_height/2);
-                    pth.lineTo(x0_triangle, h);
-                    p.setShader(new LinearGradient(x0_triangle, 0, x0_triangle + mountain_width, 0, 0x99dcedc8, 0x99689f38, Shader.TileMode.CLAMP));
-                    canvas.drawPath(pth, p);
-
-                    //Draw goal
-                    int goal = randomGenerator.nextInt(h-10-mountain_top_margin) + 10;
-                    p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-                    p.setColor(0x55ff0000);
-                    p.setStyle(Paint.Style.FILL);
-                    canvas.drawCircle(x0_triangle + mountain_width/2, layout_height/2, mountain_width/5, p);
+                //Draw goal
+                p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
+                p.setStyle(Paint.Style.FILL);
+                if(i>NUM_MOUNTAINS-3) {
+                    p.setColor(0x44ff0000);
+                    canvas.drawCircle(x0_triangle + mountain_width/2, h-Goals_data[i], mountain_width/5, p);
                 }
                 else {
-                    x0_triangle = i * mountain_width * 2 / 3;
-
-                    //Draw mountain
-                    p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-                    pth = new Path();
-                    triangle_height = randomGenerator.nextInt(h-10-mountain_top_margin) + 10; //random number between 10 and reward icon height
-                    pth.moveTo(x0_triangle, h);
-                    pth.lineTo(x0_triangle + mountain_width, h);
-                    pth.lineTo(x0_triangle + mountain_width/2, h - triangle_height);
-                    pth.lineTo(x0_triangle, h);
-                    p.setShader(new LinearGradient(x0_triangle, 0, x0_triangle + mountain_width, 0, 0xffdcedc8, 0xff689f38, Shader.TileMode.CLAMP));
-                    canvas.drawPath(pth, p);
-
-                    //Draw goal
-                    int goal = randomGenerator.nextInt(h-10-mountain_top_margin) + 10;
-                    p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
                     p.setColor(0x99ff0000);
-                    p.setStyle(Paint.Style.FILL);
-                    canvas.drawCircle(x0_triangle + mountain_width/2, h-goal, mountain_width/10, p);
+                    canvas.drawCircle(x0_triangle + mountain_width/2, h-Goals_data[i], mountain_width/10, p);
                 }
-
-//                //Draw diamond as reward if triangle is higher than goal
-//                if(triangle_height > goal) {
-//                    p = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-//                    p.setColor(Color.RED);
-////                    canvas.drawBitmap(b, left, top, p);
-//                    canvas.drawBitmap(b,x0_triangle + triangle_width/2 - b.getWidth()/2  , h - triangle_height - b.getHeight(), p);
-//                }
+            }
                 //TODO replace dot with diamond
                 //TODO ice peak random generated
                 //TODO round mountains???
                 //TODO add the day selector and change info based on the selected day
                 //TODO add days and add "this week" "two weeks ago"
                 //TODO goal setting
-            }
-
-
-
-
-
-//            Paint paint = new Paint();
-//            paint.setColor(Color.BLUE);
-//            paint.setStrokeWidth(10);
-//            paint.setStyle(Paint.Style.STROKE);
-//            paint.setShader(new LinearGradient(0, 0, 0, getHeight(), Color.BLACK, Color.WHITE, Shader.TileMode.MIRROR));
-//            canvas.drawCircle(49900, 100, 50, paint);
-//
-//
-//
-//            paint.setColor(android.graphics.Color.RED);
-//            paint.setStyle(Paint.Style.FILL_AND_STROKE);
-//            paint.setAntiAlias(true);
-//
-//
-//            Point a = new Point(49800, 0);
-//            Point b = new Point(49800, 100);
-//            Point c = new Point(49887, 50);
-//
-//            Path path = new Path();
-//            path.setFillType(Path.FillType.EVEN_ODD);
-//            path.lineTo(b.x, b.y);
-//            path.lineTo(c.x, c.y);
-//            path.lineTo(a.x, a.y);
-//            path.close();
-//
-//            canvas.drawPath(path, paint);
-
         }
+    }
+
+    /**
+     *
+     */
+    public boolean onTouch(View arg0, MotionEvent arg1) {
+        // TODO Auto-generated method stub
+        Log.i(TAG,"Touch event: "+arg1.toString());
+        Log.i(TAG, "Scroll is: " + hsv.getScrollX());
+        Log.i(TAG, "Sum is: "+(float)(hsv.getScrollX()+arg1.getX()));
+        PA_data[NUM_MOUNTAINS-2]= (int)arg1.getY();
+        Goals_data[NUM_MOUNTAINS-2]= (int)arg1.getY();
+        PA_data[NUM_MOUNTAINS-1]=(int) (layout_height-arg1.getY());
+        Goals_data[NUM_MOUNTAINS-1]=(int) (layout_height-arg1.getY());
+        mv.invalidate();
+        return false;
+    }
+    /**
+     *
+     */
+    private void generatePAdata(){
+        Random randomGenerator = new Random();
+        for(int i=0;i<NUM_MOUNTAINS-2;i++) {
+            PA_data[i] = randomGenerator.nextInt((layout_height) - 10 - mountain_top_margin) + 10; //random number
+            Goals_data[i] = randomGenerator.nextInt((layout_height) - 10 - mountain_top_margin) + 10; //random number
+        }
+        PA_data[NUM_MOUNTAINS-2]=layout_height/2;
+        Goals_data[NUM_MOUNTAINS-2]=layout_height/2;
+        PA_data[NUM_MOUNTAINS-1]=layout_height/2;
+        Goals_data[NUM_MOUNTAINS-1]=layout_height/2;
     }
 }
 
