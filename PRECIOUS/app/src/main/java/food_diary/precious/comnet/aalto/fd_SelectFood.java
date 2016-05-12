a20d7cf72b382b4703c5f3299d5fd1c24cf34f4e
todor.a.ginchev@aalto.fi <todor.a.ginchev@aalto.fi>
2016-05-12 23:20:13 +0300

    Spanish DB operational, added energy value

--- PRECIOUS/app/src/main/java/food_diary/precious/comnet/aalto/fd_SelectFood.java
+++ PRECIOUS/app/src/main/java/food_diary/precious/comnet/aalto/fd_SelectFood.java
@@ -2,6 +2,7 @@ package food_diary.precious.comnet.aalto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,13 +23,14 @@ public class fd_SelectFood extends AppCompatActivity {
 
         //Init automplete edit text
         String str[]= getResources().getStringArray(R.array.food_names);
-        AutoCompleteTextView tvAuto = (AutoCompleteTextView)
+        final AutoCompleteTextView tvAuto = (AutoCompleteTextView)
                 findViewById(R.id.autoCompleteTextView1);
         ArrayAdapter<String> adp=new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line,str);
         tvAuto.setThreshold(1);
         tvAuto.setAdapter(adp);
         //Init food composition TextViews
+        final TextView tvEnerc1000 = (TextView) findViewById(R.id.tvEnerc1000);
         final TextView tvCho = (TextView) findViewById(R.id.tvCho);
         final TextView tvFasat = (TextView) findViewById(R.id.tvFasat);
         final TextView tvFats = (TextView) findViewById(R.id.tvFats);
@@ -41,17 +43,26 @@ public class fd_SelectFood extends AppCompatActivity {
             @SuppressWarnings("all")
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    tvCho.setText(getString(R.string.cho)+": "+getResources().getIntArray(R.array.food_db_cho)[position]/1000000+"g");
-                    tvFasat.setText(getString(R.string.fasat)+": "+getResources().getIntArray(R.array.food_db_cho)[position]/1000000+"g");
-                    tvFats.setText(getString(R.string.fat)+": "+getResources().getIntArray(R.array.food_db_fats)[position]/1000000+"g");
-                    tvNa1000.setText(getString(R.string.na)+": "+getResources().getIntArray(R.array.food_db_na1000)[position]/1000+"mg");
-                    tvProt.setText(getString(R.string.prot)+": "+getResources().getIntArray(R.array.food_db_prot)[position]/1000000+"g");
-                    tvSugar.setText(getString(R.string.sugar)+": "+getResources().getIntArray(R.array.food_db_sugar)[position]/1000000+"g");
+                String str[]= getResources().getStringArray(R.array.food_names);
+                String selectedFood = tvAuto.getText().toString();
+                int index = -1;
+                for (int i=0;i<str.length;i++) {
+                    if (str[i].equals(selectedFood)) {
+                        index = i;
+                        break;
+                    }
+                }
+                Log.i(TAG, "Id=_" + index);
+
+                tvEnerc1000.setText(getString(R.string.enerc) + ": " + (double)(getResources().getIntArray(R.array.food_db_enerc1000)[index] / 1000) + "kJ");
+                    tvCho.setText(getString(R.string.cho)+": "+(double)getResources().getIntArray(R.array.food_db_cho)[index]/1000000+"g");
+                    tvFasat.setText(getString(R.string.fasat)+": "+(double)getResources().getIntArray(R.array.food_db_fasat)[index]/1000000+"g");
+                    tvFats.setText(getString(R.string.fat)+": "+(double)getResources().getIntArray(R.array.food_db_fats)[index]/1000000+"g");
+                    tvNa1000.setText(getString(R.string.na)+": "+(double)getResources().getIntArray(R.array.food_db_na1000)[index]/1000+"mg");
+                    tvProt.setText(getString(R.string.prot)+": "+(double)getResources().getIntArray(R.array.food_db_prot)[index]/1000000+"g");
+                    tvSugar.setText(getString(R.string.sugar)+": "+(double)getResources().getIntArray(R.array.food_db_sugar)[index]/1000000+"g");
             }
         });
-
 //        int activity_id = getResources().getIdentifier(ActivityTypeNoSpace, "array", this.getPackageName());
-
     }
-
 }
