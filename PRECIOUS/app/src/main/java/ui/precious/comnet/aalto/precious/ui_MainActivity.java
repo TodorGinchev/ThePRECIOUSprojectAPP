9787f498c16b405d41ac38bde3dbf1187d9c1fdd
todor.a.ginchev@aalto.fi <todor.a.ginchev@aalto.fi>
2016-06-09 14:49:38 +0300

    v2.29 permission fixes, again

--- PRECIOUS/app/src/main/java/ui/precious/comnet/aalto/precious/ui_MainActivity.java
+++ PRECIOUS/app/src/main/java/ui/precious/comnet/aalto/precious/ui_MainActivity.java
@@ -5,14 +5,19 @@
 
 package ui.precious.comnet.aalto.precious;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -26,6 +31,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Vector;
 
@@ -46,6 +52,7 @@ public class ui_MainActivity extends AppCompatActivity
     public static final String UP_PREFS_NAME = "UploaderPreferences";
     public static final String UI_PREFS_NAME = "UIPreferences";
     public static final int ONBOARDING_RESULT_CODE = 1012;
+    final private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 124;
     private SharedPreferences preferences;
     public static Context mContext;
     public static DBHelper dbhelp;
@@ -110,6 +117,7 @@ public class ui_MainActivity extends AppCompatActivity
     @Override
     public void onResume(){
         super.onResume();
+        askForPermissions();
         //Check if user has logged in
         if(  !(preferences.getBoolean("isUserLoggedIn",false)) ) {
             dbhelp.dropAllTables();
@@ -448,5 +456,65 @@ public class ui_MainActivity extends AppCompatActivity
             }
         }
     }
-}
 
+    public void askForPermissions() {
+// Here, thisActivity is the current activity
+
+        boolean hasPermission = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+
+        if(hasPermission){
+            Log.i(TAG,"WRITE_EXTERNAL_STORAGE permission granted");
+        }
+        else{
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions((Activity)this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, getString(R.string.storage_permission_warning), Toast.LENGTH_LONG).show();
+                    askForPermissions();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
