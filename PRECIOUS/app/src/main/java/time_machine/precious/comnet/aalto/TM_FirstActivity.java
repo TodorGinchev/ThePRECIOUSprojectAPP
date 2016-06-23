6c7fadae5711578ac7eb07dfcf025a1f8ac42c24
todor.a.ginchev@aalto.fi <todor.a.ginchev@aalto.fi>
2016-06-23 16:56:41 +0300

    Time machine, first screen done

--- PRECIOUS/app/src/main/java/time_machine/precious/comnet/aalto/TM_FirstActivity.java
+++ PRECIOUS/app/src/main/java/time_machine/precious/comnet/aalto/TM_FirstActivity.java
@@ -7,8 +7,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -23,41 +26,117 @@ public class TM_FirstActivity extends Fragment  {
     public static final String TM_PREFS_NAME = "TM_Preferences";
     public static SharedPreferences preferences;
     public Spinner spinnerYear;
+    public Button bYes;
+    public Button bNo;
+    public TextView tvYear;
     private View v;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         v = inflater.inflate(R.layout.tm_layout1, null);
         preferences = this.getActivity().getSharedPreferences(TM_PREFS_NAME, 0);
+        bYes = (Button) v.findViewById(R.id.buttonYes);
+        bYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("tm_remember_last_time",true);
+                editor.commit();
+                updateView();
+            }
+        });
+        bNo = (Button) v.findViewById(R.id.buttonNo);
+        bNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("tm_remember_last_time",false);
+                editor.commit();
+                updateView();
+            }
+        });
+        tvYear = (TextView) v.findViewById(R.id.textView5);
         spinnerYear = (Spinner) v.findViewById(R.id.spinner);
+        initSpinner();
         updateView();
+
+        spinnerYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(preferences.getBoolean("tm_remember_last_time",false)) {
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("tm_last_time_year", spinnerYear.getSelectedItem().toString());
+                    Log.i(TAG, "tm_last_time_year=" + spinnerYear.getSelectedItem().toString());
+//                editor.putInt("pa_soc_2nd_act_selection",-1);
+                    editor.apply();
+
+                    String text = getResources().getString(R.string.tm_1st_screen_promt_yes_and_year);
+                    tvYear.setText(String.format(text, spinnerYear.getSelectedItem().toString()));
+                    updateView();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         return v;
     }
 
-    public void updateView(){
+    public void initSpinner(){
         List<String> list = new ArrayList<String>();
         SharedPreferences up_preferences = this.getActivity().getSharedPreferences(upUtils.PREFS_NAME, 0);
         String birthdate = up_preferences.getString("birthdate", "-1");
         Log.i(TAG, "birthdate=" + birthdate);
-        int yearOfBirth=-1;
-        try{
-            Log.i(TAG,"YEAR OF BIRTH="+birthdate.substring(0,4));
-            yearOfBirth=Integer.parseInt(birthdate.substring(0,4));
-        }catch (Exception e){
-            Log.e(TAG," ",e);
+        int yearOfBirth = -1;
+        try {
+            Log.i(TAG, "YEAR OF BIRTH=" + birthdate.substring(0, 4));
+            yearOfBirth = Integer.parseInt(birthdate.substring(0, 4));
+        } catch (Exception e) {
+            Log.e(TAG, " ", e);
             return;
         }
-        if(yearOfBirth==-1)
+        if (yearOfBirth == -1)
             return;
         Calendar c_aux = Calendar.getInstance();
         c_aux.setTimeInMillis(System.currentTimeMillis());
         int currentYear = c_aux.get(Calendar.YEAR);
-        for(int i=currentYear;i>=yearOfBirth;i--){
+        list.add(" ");
+        for (int i = currentYear; i >= yearOfBirth; i--) {
             list.add(String.valueOf(i));
         }
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this.getActivity(),
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerYear.setAdapter(dataAdapter);
+    }
+
+    public void updateView(){
+        TextView tv4 = (TextView) v.findViewById(R.id.textView4);
+        TextView tv5 = (TextView) v.findViewById(R.id.textView5);
+        if(preferences.getBoolean("tm_remember_last_time",false)) {
+            tv4.setVisibility(View.VISIBLE);
+            try {
+                if (spinnerYear.getSelectedItem().toString().equals(" "))
+                    tv5.setVisibility(View.INVISIBLE);
+                else
+                    tv5.setVisibility(View.VISIBLE);
+            }catch (Exception e){
+                Log.e(TAG," ",e);
+                tv5.setVisibility(View.INVISIBLE);
+            }
+            spinnerYear.setVisibility(View.VISIBLE);
+            bYes.setBackgroundColor(getResources().getColor(R.color.mountainNotAchieved_start));
+            bNo.setBackgroundColor(getResources().getColor(R.color.checkbox_selected_background));
+
+        }
+        else{
+            tv4.setVisibility(View.INVISIBLE);
+            tv5.setVisibility(View.INVISIBLE);
+            spinnerYear.setVisibility(View.INVISIBLE);
+            bNo.setBackgroundColor(getResources().getColor(R.color.mountainNotAchieved_start));
+            bYes.setBackgroundColor(getResources().getColor(R.color.checkbox_selected_background));
+        }
     }
     /**
      *
