275e5ce6856dc7a798266487acdc997ff8f8cb4c
Todor Ginchev <todor.a.ginchev@aalto.fi>
2016-10-03 23:40:17 +0300

    prohibit OG4 screen selection for UniHelisnki trials

--- PRECIOUS/app/src/main/java/outcomegoal/precious/comnet/aalto/OGForthActivity.java
+++ PRECIOUS/app/src/main/java/outcomegoal/precious/comnet/aalto/OGForthActivity.java
@@ -15,13 +15,17 @@ import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import aalto.comnet.thepreciousproject.R;
 
 public class OGForthActivity extends Fragment {
     public static final String TAG = "OGForthActivity";
     public static final String PREFS_NAME = "OGsubappPreferences";
+    public static final String UP_PREFS_NAME = "UploaderPreferences";
     public static SharedPreferences preferences;
+    public static int groupID;
+    private static Context mContext;
     private static View v;
     MyReceiver r;
     private static RadioGroup rg;
@@ -29,22 +33,40 @@ public class OGForthActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         v = inflater.inflate(R.layout.og_layout4, null);
         preferences = this.getActivity().getSharedPreferences(PREFS_NAME, 0);
+        mContext=this.getContext();
         rg = (RadioGroup) v.findViewById(R.id.radioGroupBehaviour);
         rg.clearCheck();
-//        if(preferences.getInt("PrefferedBehaviour",-1)!=-1)
-//            rg.check(preferences.getInt("PrefferedBehaviour",-1));
+
+        SharedPreferences preferences_up = this.getActivity().getSharedPreferences(UP_PREFS_NAME, 0);
+        groupID = preferences_up.getInt("group_ID", -1);
+
+
+        if(preferences.getInt("PrefferedBehaviour",-1)!=-1)
+            rg.check(preferences.getInt("PrefferedBehaviour",-1));
+
+
+        if(groupID/1000==9){
+            rg.check(R.id.radioOption1);
+            updateView();
+        }
+
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Log.i("OGForthActivity","CHECKED_"+checkedId);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt("PrefferedBehaviour",checkedId);
-                editor.apply();
+                if(groupID/1000==9){
+                    Toast.makeText(mContext,mContext.getResources().getString(R.string.og_4th_screen_not_allowed),Toast.LENGTH_SHORT).show();
+                    group.check(R.id.radioOption1);
+
+                }
+                else {
+                    Log.i("OGForthActivity", "CHECKED_" + checkedId);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putInt("PrefferedBehaviour", checkedId);
+                    editor.apply();
+                }
 
             }
         });
-        if(preferences.getInt("PrefferedBehaviour",-1)!=-1)
-            rg.check(preferences.getInt("PrefferedBehaviour",-1));
 
         return v;
     }
